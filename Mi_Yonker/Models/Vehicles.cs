5204d349f0715c293ea45c3072d66ca4d68ea836
Javier Argueta <javychavez10@gmail.com>
2020-08-28 02:10:42 -0600

    Creación de Models-Vehicles

---
+++ Mi_Yonker/Models/Vehicles.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Mi_Yonker.Models
+{
+    public class Vehicles
+    {
+        [ForeignKey("id")]
+        public int id { get; set; }
+        public int brand { get; set; }
+        public int model { get; set; }
+        public int year { get; set; }
+        public virtual Models Models { get; set; }
+    }
+}
