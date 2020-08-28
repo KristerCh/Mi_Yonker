d58541bd9533f2dd0340952b1da75e319d6d4de4
KristerCh <KBMChavez@outlook.com>
2020-08-27 22:48:40 -0600

    Se creo el modelo "Brand"

---
+++ Mi_Yonker/Models/Brands.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mi_Yonker.Models
+{
+    public class Brands
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+    }
+}
