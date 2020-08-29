3c8df2b6b161499ed9665f0586a33b3c6d4ba3c2
JAMF\CASA <Jeffrey_alexander@unitec.edu>
2020-08-28 23:21:44 -0600

    ModeloPartlis_PartlisDetails_Added

---
+++ Mi_Yonker/Models/Partlist.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mi_Yonker.Models
+{
+    public class Partlist
+    {
+        [Key]
+        public int id { get; set; }
+        public string name { get; set; }
+    }
+}
---
+++ Mi_Yonker/Models/Partlist_details.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mi_Yonker.Models
+{
+    public class Partlist_details
+    {
+        [Key]
+        public int id { get;set; }
+        [ForeignKey("idpart")]
+        public virtual Partlist Partlist { get; set; } 
+        public int idpart { get; set; }
+        public int detail { get; set; }
+
+    }
+}
