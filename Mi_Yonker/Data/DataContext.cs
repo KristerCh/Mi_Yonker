34d34cd1dc384b360c6590d2158bd11a8af04684
KristerCh <KBMChavez@outlook.com>
2020-08-29 00:31:57 -0600

    Renamed folder "Models" to "Modelos"

--- Mi_Yonker/Data/DataContext.cs
+++ Mi_Yonker/Data/DataContext.cs
@@ -1,11 +1,19 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Mi_Yonker.Data
 {
-    public class DataContext
+    public class DataContext : DbContext   
     {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsbuilder)
+        {
+            optionsbuilder.UseSqlServer(@"Server=KRISTER\MI_EQUIPO;Database=MiYonker");
+        }
+
+        public DbSet<Brands> brands { get; set; }
+        public DbSet<Models>
     }
 }
