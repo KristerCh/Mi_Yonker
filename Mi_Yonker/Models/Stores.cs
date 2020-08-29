48b3e5f2fc41250f6f31d8036dd0dc103991ef93
wendyny <wenyen9306@gmail.com>
2020-08-28 22:11:13 -0600

    Modelos_Stores_Users_28-08-2020

---
+++ Mi_Yonker/Models/Stores.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+
+namespace Mi_Yonker.Models
+{
+    public class Stores
+    {
+        [Key]
+        public int rtn { get; set; }
+        public string name { get; set; }
+        public string address { get; set; }
+        public float latitue { get; set; }
+        public float longitude { get; set; }
+        public string owner { get; set; }
+        public int owner_id { get; set; }
+        public string contact_person { get; set; }
+        public string phone { get; set; }
+        public string email { get; set; }
+        public string facebook { get; set; }
+        public string website { get; set; }
+    }
+}
